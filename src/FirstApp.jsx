299a3a4fb8e66be1a3de15526174d1d0021298eb
Firthuns2021 <86574425+Firthuns2021@users.noreply.github.com>
2022-06-15 18:51:56 +0200

    Initial commit

---
+++ src/FirstApp.jsx
@@ -0,0 +1,40 @@
+
+
+export const FirstApp = () => {
+    return (
+        <>
+            <h1>{ 1 + 1  }</h1>
+            <h3>Soy Fernando</h3>
+            <p>Soy un subtitulo</p>
+        </>
+    )
+}
+
+// import PropTypes from 'prop-types';
+//
+// export const FirstApp = ({ title, subTitle, name }) => {
+//
+//     // console.log(props);
+//
+//     return (
+//         <>
+//             <h1 data-testid="test-title"> { title } </h1>
+//             {/* <code>{ JSON.stringify( newMessage ) }</code> */}
+//             <p>{ subTitle }</p>
+//             <p>{ subTitle }</p>
+//             <p>{ name }</p>
+//         </>
+//     )
+// }
+//
+//
+// FirstApp.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     subTitle: PropTypes.string,
+// }
+//
+// FirstApp.defaultProps = {
+//     name: 'Fernando Herrera',
+//     subTitle: 'No hay subtítulo',
+//     // title: 'No hay título',
+// }
